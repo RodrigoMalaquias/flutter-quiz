c4fffaf515bd9c1ff9425b83a1d57c3d7d1918f2
RodrigoMalaquias <rodrigomalaquias5@gmail.com>
2024-12-11 01:43:26 -0300

    Updating yaml with the assets of the results images

--- lib/result.dart
+++ lib/result.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-
   final int score;
   final void Function() onRestartQuiz;
 
   Result(this.score, this.onRestartQuiz);
 
   String get textResult {
-    if(score < 8){
+    if (score < 8) {
       return 'Congratulations!';
     } else if (score < 12) {
       return 'You are good!';
-    } else if (score < 16){
+    } else if (score < 16) {
       return 'Impressive!';
     } else {
       return 'God level!';
+    }
+  }
+
+  String get imageAsset {
+    if (score < 8) {
+      return 'assets/congratulations.png';
+    } else if (score < 12) {
+      return 'assets/good.png';
+    } else if (score < 16) {
+      return 'assets/impressive.png';
+    } else {
+      return 'assets/god_level.png';
     }
   }
 
@@ -25,15 +36,22 @@ class Result extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Center(
-            child: Text(
+          child: Text(
             textResult,
             style: TextStyle(fontSize: 28),
-            ),
+          ),
+        ),
+        Center(
+          child: Image.asset(
+            imageAsset,
+            width: 200,
+            height: 200,
+          ),
         ),
         FloatingActionButton(
           onPressed: onRestartQuiz,
-          child: Icon(Icons.refresh)
-          )
+          child: Icon(Icons.refresh),
+        ),
       ],
     );
   }
